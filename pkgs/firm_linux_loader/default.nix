507c289002becced6a8cf34ae11db5c787d8abf6
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-07-31 16:38:10 -0400

    firm_linux_loader: Init at 2021-05-13

---
+++ pkgs/firm_linux_loader/default.nix
@@ -0,0 +1,32 @@
+{ stdenv
+, fetchFromGitHub
+, gcc-arm-embedded
+, buildPackages
+}:
+
+stdenv.mkDerivation {
+  pname = "firm_linux_loader";
+  version = "unstable-2021-05-13";
+
+  nativeBuildInputs = [
+    gcc-arm-embedded
+    buildPackages.ctr-packages.firmtool
+  ];
+
+  makeFlags = [
+    "CC=arm-none-eabi-gcc"
+  ];
+
+  src = fetchFromGitHub {
+    owner = "linux-3ds";
+    repo = "firm_linux_loader";
+    rev = "4098b022833bb794383cbf269d6e39a887fc3354";
+    sha256 = "0mh1iw1l8a69rx4lxkmmpnhw5rpz11lsn2iyg31a3gyikg8frap3";
+  };
+
+  installPhase = ''
+    mkdir -p $out
+    cp -t $out \
+      firm_linux_loader.firm
+  '';
+}
