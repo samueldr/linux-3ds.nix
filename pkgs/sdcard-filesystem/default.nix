79d3886ee4cd8a4bf752aeee685b928f4624fb12
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-08-01 16:17:05 -0400

    sdcard-filesystem: Allow for providing an initramfs

--- pkgs/sdcard-filesystem/default.nix
+++ pkgs/sdcard-filesystem/default.nix
@@ -1,4 +1,8 @@
-{ runCommandNoCC, ctr-packages }:
+{ lib
+, runCommandNoCC
+, ctr-packages
+, initramfs ? null
+}:
 
 # Paths:
 #  - https://github.com/linux-3ds/firm_linux_loader/blob/4098b022833bb794383cbf269d6e39a887fc3354/common/linux_config.h#L8-L11
@@ -8,6 +12,7 @@ runCommandNoCC "sdcard-filesystem" { } ''
     ${ctr-packages.linux}/zImage \
     ${ctr-packages.linux}/dtbs/* \
     ${ctr-packages.arm9linuxfw}/arm9linuxfw.bin
+  ${lib.optionalString (initramfs != null) "cp -v ${initramfs} $out/linux/initramfs.cpio.gz"}
   mkdir -p $out/luma/payloads/
   cp -vt $out/luma/payloads/ \
     ${ctr-packages.firm_linux_loader}/firm_linux_loader.firm
